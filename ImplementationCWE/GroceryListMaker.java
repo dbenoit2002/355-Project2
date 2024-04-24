fd896f52e7aabb9b1b5eb55aa433972d666ed0e3
Brian Nicolellis <briannicolellis@prodigy.net>
2024-04-24 14:07:52 -0500

    Created login menu

--- ImplementationCWE/GroceryListMaker.java
+++ ImplementationCWE/GroceryListMaker.java
@@ -1,5 +1,61 @@
 package ImplementationCWE;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class GroceryListMaker {
-    
+    public static void main(String args[]) {
+        if(new GroceryListMaker().loginMenu() != null) {
+
+        }
+    }
+
+    public User loginMenu() {
+        boolean endFlag = false;
+        Scanner scanner = new Scanner(System.in);
+        int userInput;
+        User curUser = null;
+        FileHandler fileHandler = new FileHandler();
+        
+        while(!endFlag) {
+            System.out.println("Select an option: ");
+            System.out.println("0: Exit");
+            System.out.println("1: Create an account");
+            System.out.println("2: Login");
+            try{
+                userInput = Integer.parseInt(scanner.nextLine());
+                if(userInput < 0 || userInput > 2) {
+                    System.out.println("Error: Please enter a valid option");
+                }
+                else if(userInput == 0) {
+                    endFlag = true;
+                }
+                else {
+                    System.out.println("Enter a username: ");
+                    String userName = scanner.nextLine();
+                    System.out.println("Enter a password: ");
+                    String password = scanner.nextLine();
+                    if(userInput == 1) {
+                        curUser = new User(userName, password);
+                        fileHandler.writeUser(curUser);
+                        return curUser;
+                    }
+                    else if(userInput == 2) {
+                        User fileUser = fileHandler.readUser();
+                        if(fileUser != null && fileUser.getUserName().equals(userName) &&
+                           fileUser.getPassword().equals(password)) {
+                            curUser = new User(userName, password);
+                            return curUser;
+                        } else {
+                            System.out.println("Error: Incorrect user name or password");
+                        }
+                    }
+                }
+            } catch(NumberFormatException e) {
+                System.out.println("Error: Please enter a number");
+            }
+        }
+        scanner.close();
+        return null;
+    }
 }
